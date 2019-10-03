bbefe33cbcbe8731ea03e4b2991f9084dee1e94d
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2019-10-03 03:59:30 +0000

    ingredients loop

---
+++ ingredients.js
@@ -0,0 +1,13 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+let w = 0;
+while(w < ingredients.length){
+	console.log(ingredients[w]);
+	w++;
+}
+
+for(i = 0;i < ingredients.length;i++){
+	console.log(ingredients[i]);
+}
+for(i = ingredients.length - 1;i >= 0;i--){
+	console.log(ingredients[i]);
+}
